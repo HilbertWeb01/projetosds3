0c39fd15ab33b2803151c8089674695d30d7075a
HilbertWeb01 <93559380+HilbertWeb01@users.noreply.github.com>
2021-11-02 15:02:06 -0200

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://syshilsoftware.com.br" target="_blank" rel="noreferrer">Hilbert Muniz</a></p>
+                <p className="text-light"><small><strong>SyS HiL Software</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/hilbertmunizde/" target="_blank" rel="noreferrer">hilbertmunizde</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
